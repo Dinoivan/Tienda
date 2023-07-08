449d43ca01ef6509736c167cf678739b320b76f8
Dino Iván <122911168+Dinoivan@users.noreply.github.com>
2023-07-07 22:04:48 -0500

    Products

--- src/components/pages/Products.jsx
+++ src/components/pages/Products.jsx
@@ -1,11 +1,10 @@
 
-
 const Products = () =>{
     return(
     <>
         <div>
             <h1>Products</h1>
-            <p>Explora todos nuestros productos en linea</p>
+            <p>Explora todos nuestros productos en linea!</p>
         </div>
     </>
     )
