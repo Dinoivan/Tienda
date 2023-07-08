7fa17a3f48ef65a6c5af2a0cdf09a76fdc267afc
Dino Iván <122911168+Dinoivan@users.noreply.github.com>
2023-07-07 21:35:24 -0500

    React-router-dom

---
+++ src/components/molecules/header/Logo.jsx
@@ -0,0 +1,9 @@
+const Logo = () => {
+    return(
+        <div>
+            <img src="https://1000marcas.net/wp-content/uploads/2021/05/SAP-Logo.png" alt="Logo de SAP"/>
+        </div>
+    )
+}
+
+export default Logo
